5225b87b603eb83b9a181041b03381d06adf5041
Sophia Filimonova <sophfil708@gmail.com>
2023-05-25 14:58:11 +0300

    add CRUD create

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -5,4 +5,7 @@ from hexlet_django_blog.article import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='articles_index'),
     path('<slug:tags>/<int:article_id>/', views.index, name='article'),
+    path('<int:id>/', views.ArticleView.as_view(), name='show_article'),
+    path('create/', views.ArticleFormCreateView.as_view(),
+         name='articles_create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,9 +3,10 @@
 # Create your views here.
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
 
 
 class IndexView(View):
@@ -16,5 +17,30 @@ class IndexView(View):
                       context={'articles': articles})
 
 
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article,
+                                    id=kwargs['id'])
+        return render(request, 'articles/show.html',
+                      context={'article': article,})
+
 def index(request, tags, article_id):
     return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
+
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html',
+                      {'form': form,})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles_index')
+
+        return render(request, 'articles/create.html',
+                      {'form': form,})
